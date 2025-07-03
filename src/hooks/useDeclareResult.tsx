0806551de2a87875901b85b60d273496a9f287bc
Lavkush <lkyadav090@gmail.com>
2025-07-03 18:19:02 +0530

    modal visibility fixed post result declaration

--- src/hooks/useDeclareResult.tsx
+++ src/hooks/useDeclareResult.tsx
@@ -2,10 +2,12 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useAxios from "./useAxios";
 import { API_ROUTES } from "../_common/api-routes";
 import { QUERY_KEYS } from "../_common/query-keys";
+import { useModelStore } from "../state/useModelStore";
 
 function useDeclareResult() {
   const { postData } = useAxios();
   const queryClient = useQueryClient();
+  const { setIsDeclareResultModalVisible } = useModelStore();
   return useMutation({
     mutationFn: ({
       marketId,
@@ -21,6 +23,7 @@ function useDeclareResult() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.MARKETS] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.USER] });
+      setIsDeclareResultModalVisible(false);
     },
   });
 }
